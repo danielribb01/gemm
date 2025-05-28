13847b0bbd3a7b11c809bd736e0c458713d9ceff
Gabriel Valentin <gabriel.valentin@neospace.ai>
2025-05-28 17:40:35 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,21 +2,25 @@ from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
 setup(
-    name="neogemm",
+    name="neoGEMM",
     ext_modules=[
         CUDAExtension(
-            name="neogemm",
-            sources=["neogemm.cu"],
+            name="neoGEMM",
+            sources=["neogemmV02.cu"],
             include_dirs=[
                 "/usr/local/lib/python3.12/dist-packages/pybind11/include"  # Caminho para pybind11 headers
             ],
             extra_compile_args={
                 "cxx": ["-std=c++17"],
-                "nvcc": ["-std=c++17", "-O3"]
-            }
+                "nvcc": ["-std=c++17", "-O3", "-DNDEBUG", "-w", "-expt-relaxed-constexpr", "-U__CUDA_NO_BFLOAT16_CONVERSIONS__", "--expt-extended-lambda", "--use_fast_math", "-Xcompiler=-fPIC ", "-Xcompiler=-Wno-psabi", "-Xcompiler=-fno-strict-aliasing", "-gencode", "arch=compute_90a,code=sm_90a", "-lineinfo"]
+            },
+            libraries=["cuda", "cublas"],
+            define_macros=[("__CUDA_NO_BFLOAT16_CONVERSIONS__", "0")]
         )
     ],
     cmdclass={
         "build_ext": BuildExtension
     }
 )
+
+
