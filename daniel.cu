1ef1f0bab962a0e37d3f74b65a30ad440f35d08d
danielribb01 <daniel.ribeiro@neospace.ai>
2025-05-29 17:14:53 -0300

    Update daniel.cu

--- daniel.cu
+++ daniel.cu
@@ -90,3 +90,86 @@ __device__ void warpgroup_wait()
 }
 
 
+template<int BM, int BN, int BK, int WGMMA_M, int WGMMA_N, int WGMMA_K, int NUM_THREADS>
+__global__ void __launch_bounds__(NUM_THREADS) gemm(int M, int N, int K, bf16* C, const CUtensorMap* tensorMapA, const CUtensorMap* tensorMapB)
+{
+    __shared__ alignas(128) bf16 sA[BM * BK];
+    __shared__ alignas(128) bf16 sB[BK * BN];
+
+    float d[WGMMA_N/16][8];
+
+    static_assert(sizeof(d) * 128 == BM * BN * sizeof(float));
+    memset(d, 0, sizeof(d));
+
+    const int num_blocks_k = K / BK;
+    int num_block_n = blockIdx.x % (N / BN);
+    int num_block_m = blockIdx.x / (N / BN);
+    #pragma nv_diag_suppress static_var_with_dynamic_init
+    __shared__ barrier barA;
+    __shared__ barrier barB;
+
+    if(threadIdx.x == 0){
+        init(&barA, blockDim.x);
+        init(&barB, blockDim.x);
+        cde::fence_proxy_async_shared_cta();
+    }
+    __syncthreads();
+
+    barrier::arrival_token tokenA, tokenB;
+    for(int block_k_iter = 0; block_k_iter < num_blocks_k; ++ block_k_iter)
+    {
+        if(threadIdx.x == 0){
+            cde::cp_async_bulk_tensor_2d_global_to_shared(&sA[0], tensorMapA, block_k_iter*BK, num_block_m*BM, barA);
+            tokenA = cuda::device::barrier_arrive_tx(barA, 1, sizeof(sA));
+            cde::cp_async_bulk_tensor_2d_global_to_shared(&sB[0], tensorMapB, block_k_iter*BK, num_block_n*BN, barB);
+            tokenB = cuda::device::barrier_arrive_tx(barB, 1, sizeof(sB));
+        }else{
+            tokenA = barA.arrive();
+            tokenB = barB.arrive();
+        }
+
+        barA.wait(std::move(tokenA));
+        barB.wait(std::move(tokenB));
+        __syncthreads();
+
+        warpgroup_arrive();
+        wgmma64<1,1,1,0,0>(d, &sA[0], &sB[0]);
+        wgmma64<1,1,1,0,0>(d, &sA[WGMMA_K], &sB[WGMMA_K]);
+        wgmma64<1,1,1,0,0>(d, &sA[WGMMA_K * 2], &sB[2 * WGMMA_K]);
+        wgmma64<1,1,1,0,0>(d, &sA[WGMMA_K * 3], &sB[WGMMA_K * 3]);
+        warpgroup_commit_batch();
+        warpgroup_wait<0>();
+
+    }
+
+    {
+        int tid = threadIdx.x;
+        int lane = tid % 32;
+        int warp = tid / 32;
+        uint32_t row = warp*16 + lane / 4;
+        bf16 *block_C = C + num_block_n*BN*M + num_block_m*BM;
+
+        for(int m_it = 0; m_it<BM/WGMMA_M; ++m_it){
+            for(int n_it = 0; n_it<BN/WGMMA_N; ++n_it){
+                for(int w = 0; w < WGMMA_N/16; ++w){
+                    int col = 16*w + 2*(tid % 4);
+                    #define IDX(i, j) ((j + n_it*WGMMA_N) * M + ((i) + m_it * WGMMA_M))
+                   
+                    block_C[IDX(row, col)] = __float2bfloat16(d[w][0]);
+                    block_C[IDX(row, col+1)] = __float2bfloat16(d[w][1]);
+                    block_C[IDX(row+8, col)] = __float2bfloat16(d[w][2]);
+                    block_C[IDX(row+8, col+1)] = __float2bfloat16(d[w][3]);
+    
+                    block_C[IDX(row, col+8)] = __float2bfloat16(d[w][4]);
+                    block_C[IDX(row, col+9)] = __float2bfloat16(d[w][5]);
+                    block_C[IDX(row+8, col+8)] = __float2bfloat16(d[w][6]);
+                    block_C[IDX(row+8, col+9)] = __float2bfloat16(d[w][7]);
+
+                    #undef IDX                  
+                }
+            }
+        }
+    }
+
+}
+
